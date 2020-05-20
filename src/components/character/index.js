542f2668f96f6b5505040c5e9190af7d64569a72
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2020-05-20 17:50:19 -0300

    style: add character styles

--- src/components/character/index.js
+++ src/components/character/index.js
@@ -1,17 +1,21 @@
 // @flow
 import React from 'react'
 import type {Player as PlayerType, Monster as MonsterType} from '../../shared/types'
+import styles from './styles.module.scss'
 
 const Character = ({character}: {character: PlayerType | MonsterType}) =>
-    <div>
-      <div className='details'>
-        <div className='avatar' />
-        <div className='main'>
-          <div className='name'>{character.name}</div>
-          <div className='health'>HP: {character.hp}/{character.maxHp}</div>
+    <div className={styles.main}>
+      <div className={styles.details}>
+        <div
+          className={styles.avatar}
+          style={{backgroundImage: `url(${String(character.image || '') || 'https://via.placeholder.com/200x200?text=player'})`}}
+        />
+        <div className={styles.content}>
+          <div className={styles.name}>{character.name.toLowerCase()}</div>
+          <div className={styles.health}><span>HP: </span>{character.hp}/{character.maxHp}</div>
         </div>
       </div>
-      <div className='modifiers'> shield: {character.shield}</div>
+      <div className={styles.modifiers}><span>Shield: </span>{character.shield}</div>
     </div>
 
 export default Character
