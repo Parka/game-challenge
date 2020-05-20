638107a497e2df92315f01e1fd03af6b904f4586
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2020-05-20 16:12:29 -0300

    style: add login styles

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,14 +1,27 @@
 // @flow
 import React, {useState} from "react"
+import styles from './styles.module.scss'
 
 const Login = ({handleOnLoginSubmit}: {handleOnLoginSubmit: (string)=>void}) => {
   const [name, setName] = useState('')
   return (
-    <div>
-      <h1>Welcome to Bons Game</h1>
-      <h2>What's your name?</h2>
-      <input type='text' value={name} onChange={e => setName(e.target.value)}/>
-      <button onClick={() => handleOnLoginSubmit(name)}> Let's play </button>
+    <div  className={styles.main}>
+      <div  className={styles.content}>
+        <h1 className={styles.title}>Welcome to Bons Game</h1>
+        <h2 className={styles.caption}>What's your name?</h2>
+        <input
+          type='text'
+          className={styles.input}
+          value={name}
+          onChange={e => setName(e.target.value)}
+          placeholder='name'
+        />
+        <button
+          className={styles.button}
+          onClick={() => handleOnLoginSubmit(name)}>
+          Let's play
+        </button>
+      </div>
     </div>
   )
 }
