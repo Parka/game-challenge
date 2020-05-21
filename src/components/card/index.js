ea2b962035f88e932daf9af5e3358078af07519b
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2020-05-21 09:46:25 -0300

    feat: make card selectable

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -3,8 +3,14 @@ import React from 'react'
 import type {Card as CardType} from '../../shared/types'
 import styles from './styles.module.scss'
 
-const Card = ({card, onClick}: {card: CardType, onClick:any}) =>
-    <div className={styles.main} onClick={onClick}>
+type Props = {
+  card: CardType,
+  onClick:any,
+  selected: bool
+}
+
+const Card = ({card, onClick, selected}: Props) =>
+    <div className={[styles.main, selected ? styles.selected : ''].join(' ')} onClick={onClick}>
         <div className={styles.image} style={{backgroundImage: `url('https://via.placeholder.com/200x200?text=${card.effect}')`}}/>
         <div className={styles.details}>
           <div className={styles.effect}>{card.effect.toLowerCase()}</div>
--- src/game/index.js
+++ src/game/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react'
+import React, {useState} from 'react'
 import type {Game as GameType} from '../shared/types'
 import styles from './styles.module.scss'
 import Character from '../components/character'
@@ -13,32 +13,39 @@ type Props = {
   horror: bool,
 }
 
-const Game = ({disabled, game, handlePlayerAction, horror}: Props) => (
-  <div className={styles.wrapper} style={{pointerEvents: disabled ? 'none' : 'initial'}}>
-    <div className={styles.main} style={{pointerEvents: horror ? 'none' : 'initial'}}>
-      <div className={styles.characters}>
-        <Character character={game.monster} />
-        <Character character={game.player} horror={horror} />
+const Game = ({disabled, game, handlePlayerAction, horror}: Props) => {
+  const [selectedCard, setSelectedCard] = useState('')
+  return (
+    <div className={styles.wrapper} style={{pointerEvents: disabled ? 'none' : 'initial'}}>
+      <div className={styles.main} style={{pointerEvents: horror ? 'none' : 'initial'}}>
+        <div className={styles.characters}>
+          <Character character={game.monster} />
+          <Character character={game.player} horror={horror} />
+        </div>
+        <div className={styles.cards}  style={{opacity: disabled || horror ? 0.5 : 1}}>
+        {
+          game.player.cards.map(card =>
+            <Card
+              key={card.id}
+              card={card}
+              selected={card.id === selectedCard}
+              onClick={() => setSelectedCard(card.id)}
+            />
+          )
+        }
+        </div>
       </div>
-      <div className={styles.cards}  style={{opacity: disabled || horror ? 0.5 : 1}}>
-      {
-        game.player.cards.map(card =>
-          <Card
-            key={card.id}
-            card={card}
-            onClick={() => handlePlayerAction(card.id)}
-          />
-        )
-      }
+      <div className={styles.side}>
+        <Turn
+          game={game}
+          onClick={() => {
+            handlePlayerAction(selectedCard)
+            setSelectedCard('')
+          }}
+        />
       </div>
     </div>
-    <div className={styles.side}>
-      <Turn
-        game={game}
-        onClick={() => handlePlayerAction('')}
-      />
-    </div>
-  </div>
-)
+  )
+}
 
 export default Game
